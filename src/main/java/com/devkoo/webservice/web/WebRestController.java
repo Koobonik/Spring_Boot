61d61688661b60bc01275cf0f47b6021e3e30e96
Koobonik <kbi960130@naver.com>
2018-06-21 22:22:20 +0900

    주석 추가 및 다른 문구 추가

--- src/main/java/com/devkoo/webservice/web/WebRestController.java
+++ src/main/java/com/devkoo/webservice/web/WebRestController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebRestController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello") // 웹에서 /hello 라는 요청이 있을경우
     public String hello() {
         return "HelloWorld";
-    }
+    } // HelloWorld 라는 문구 반환
+
+    @GetMapping("/welcome")
+    public String welcome(){ return "Welcome to Spring Boot";} // 조금 더 응용해서 다른 문구도 출력가능하게
 }
