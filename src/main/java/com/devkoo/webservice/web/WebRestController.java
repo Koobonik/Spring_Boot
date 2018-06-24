42a96da17ca90c8fed9c3f4ed8b4f896bbb8c9f0
Koobonik <kbi960130@naver.com>
2018-06-25 03:03:18 +0900

    오류 발생중...

--- src/main/java/com/devkoo/webservice/web/WebRestController.java
+++ src/main/java/com/devkoo/webservice/web/WebRestController.java
@@ -1,10 +1,18 @@
 package com.devkoo.webservice.web;
 
+import com.devkoo.webservice.domain.posts.Posts;
+import com.devkoo.webservice.domain.posts.PostsRepository;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebRestController {
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello") // 웹에서 /hello 라는 요청이 있을경우
     public String hello() {
@@ -15,4 +23,28 @@ public class WebRestController {
     public String welcome(){
         return "Welcome to Spring Boot";
     } // 조금 더 응용해서 다른 문구도 출력가능하게
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
+    }
+
+
+}
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto{
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
 }
