fedc13842ff2f5e5fd58d3898092e1cc2a09c9f7
Koobonik <kbi960130@naver.com>
2018-06-26 22:00:34 +0900

    DB 에러..

--- src/main/java/com/devkoo/webservice/domain/posts/Posts.java
+++ src/main/java/com/devkoo/webservice/domain/posts/Posts.java
@@ -10,28 +10,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-// 기본 생성자 자동 추가 // 기본 생성자의 접근 권한을 protected로 제한
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 
-// 클래스내 모든 필드의 Getter 메소드를 자동생성
-@Getter
-
-// 테이블과 링크될 클래스 + 언더스코어 네이밍으로 이름 매칭 ex ) SalesManager -> sales_manager table
-@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 자동 추가 // 기본 생성자의 접근 권한을 protected로 제한
+@Getter // 클래스내 모든 필드의 Getter 메소드를 자동생성
+@Entity // 테이블과 링크될 클래스 + 언더스코어 네이밍으로 이름 매칭 ex ) SalesManager -> sales_manager table
 public class Posts {
     @Id // 해당 테이블의 PK 필드를 나타냄
     @GeneratedValue // PK의 생성 규칙을 나타냄
     private  Long id;
-
-
-
-
     // 테이블의 컬럼을 나타내면 굳이 선언하지 않더라도 해당 클래스의 필드는 모두 컬럼이 됨
     // 사용하는 이유는 기본값 외에 추가로 변경이 필요한 옵션이 있을경우 사용함
     // 문자열의 경우 VARCHAR(255)가 기본값인데, 사이즈를 500으로 늘리고 싶거나(ex: title), 타입을 TEXT로 변경하고 싶거나
     // (ex: content) 등의 경우에 사용됨
     @Column(length = 500, nullable = false)
     private String title;
+
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
--- src/main/java/com/devkoo/webservice/web/WebRestController.java
+++ src/main/java/com/devkoo/webservice/web/WebRestController.java
@@ -4,6 +4,7 @@ import com.devkoo.webservice.domain.posts.Posts;
 import com.devkoo.webservice.domain.posts.PostsRepository;
 import jdk.nashorn.internal.objects.annotations.Getter;
 import jdk.nashorn.internal.objects.annotations.Setter;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,8 +12,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@AllArgsConstructor
 public class WebRestController {
     private PostsRepository postsRepository;
+
+    public WebRestController(PostsRepository postsRepository){
+        this.postsRepository = postsRepository;
+    }
 
     @GetMapping("/hello") // 웹에서 /hello 라는 요청이 있을경우
     public String hello() {
@@ -27,9 +33,6 @@ public class WebRestController {
     @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto){
         postsRepository.save(dto.toEntity());
-
     }
-
-
 }
 
