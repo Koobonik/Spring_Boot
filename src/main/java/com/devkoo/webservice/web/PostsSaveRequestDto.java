a607cb9d48efbbf83b1dd88b3b10bcbcb11dade7
Koobonik <kbi960130@naver.com>
2018-06-26 18:58:59 +0900

    오류 수정~

---
+++ src/main/java/com/devkoo/webservice/web/PostsSaveRequestDto.java
@@ -0,0 +1,25 @@
+package com.devkoo.webservice.web;
+
+import com.devkoo.webservice.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto{
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
--- src/main/java/com/devkoo/webservice/web/WebRestController.java
+++ src/main/java/com/devkoo/webservice/web/WebRestController.java
@@ -27,24 +27,9 @@ public class WebRestController {
     @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto){
         postsRepository.save(dto.toEntity());
+
     }
 
 
 }
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class PostsSaveRequestDto{
-    private String title;
-    private String content;
-    private String author;
-
-    public Posts toEntity(){
-        return Posts.builder()
-                .title(title)
-                .content(content)
-                .author(author)
-                .build();
-    }
-}
