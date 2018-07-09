f45a85c70370773f01fde8d1466123121c6deed8
Koobonik <kbi960130@naver.com>
2018-07-09 23:17:47 +0900

    생성 수정

--- src/main/java/com/devkoo/webservice/service/PostsService.java
+++ src/main/java/com/devkoo/webservice/service/PostsService.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @AllArgsConstructor
 @Service
 public class PostsService {
@@ -14,5 +17,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
