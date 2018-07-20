8c9f921123bbd9a71cc02b85bd49f0eac310885c
Koobonik <kbi960130@naver.com>
2018-07-20 19:40:03 +0900

    추가

---
+++ src/test/java/com/devkoo/webservice/web/WebRestControllerTest.java
@@ -0,0 +1,33 @@
+package com.devkoo.webservice.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+/**
+ * Created by jojoldu@gmail.com on 2017. 12. 31.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class WebRestControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void Profile확인 () {
+        //when
+        String profile = this.restTemplate.getForObject("/profile", String.class);
+
+        //then
+        //assertThat(profile).isEqualTo("local");
+    }
+}
