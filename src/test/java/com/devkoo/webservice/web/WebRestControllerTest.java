4c262c48563b192a62742ff02559f2d19f05ff52
Koobonik <kbi960130@naver.com>
2018-08-01 15:07:03 +0900

    깃허브 충돌로 인해 수정 라인 삭제

--- src/test/java/com/devkoo/webservice/web/WebRestControllerTest.java
+++ src/test/java/com/devkoo/webservice/web/WebRestControllerTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.devkoo.webservice.web;
 
 import org.junit.Test;
@@ -19,45 +18,11 @@ public class WebRestControllerTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    public void Profile확인 () {
+    public void Profile확인() {
         //when
         String profile = this.restTemplate.getForObject("/profile", String.class);
 
         //then
         assertThat(profile).isEqualTo("local");
     }
-=======
-package com.devkoo.webservice.web;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
-/**
- * Created by jojoldu@gmail.com on 2017. 12. 31.
- * Blog : http://jojoldu.tistory.com
- * Github : https://github.com/jojoldu
- */
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-public class WebRestControllerTest {
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Test
-    public void Profile확인 () {
-        //when
-        String profile = this.restTemplate.getForObject("/profile", String.class);
-
-        //then
-        //assertThat(profile).isEqualTo("local");
-    }
->>>>>>> f9a816a940b3370b408b2a1481a746d921f665f3
 }
