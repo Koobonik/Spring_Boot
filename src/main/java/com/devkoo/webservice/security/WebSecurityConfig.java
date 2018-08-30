132e63894b8fa16590064aa089d590737ca28cf3
Koobonik <kbi960130@naver.com>
2018-08-31 00:32:21 +0900

    테스트코드 작성

---
+++ src/main/java/com/devkoo/webservice/security/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.devkoo.webservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Collection;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        User user = new User("user", "password", false, false, false, false, null);
+
+        return new InMemoryUserDetailsManager((Collection<UserDetails>) user);
+    }
+}
---
+++ src/test/java/com/devkoo/webservice/security/WebSecurityConfigTest.java
@@ -0,0 +1,24 @@
+package com.devkoo.webservice.security;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Collection;
+
+public class WebSecurityConfigTest {
+
+    @Autowired
+    WebSecurityConfig webSecurityConfig;
+
+    @Test
+    public UserDetailsService userDetailsService() {
+        User user = new User("user", "password", false, false, false, false, null);
+
+        return new InMemoryUserDetailsManager((Collection<UserDetails>) user);
+    }
+
+}
