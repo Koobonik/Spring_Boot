fd3c958db34c1a33d360d847376aafcde3278873
Koobonik <kbi960130@naver.com>
2018-09-06 11:27:40 +0900

    비밀번호 유효성 검사 기능 구현 완료

--- src/main/resources/static/js/app/passwordcheck.js
+++ src/main/resources/static/js/app/passwordcheck.js
@@ -1,19 +1,19 @@
 function check() {
-    document.getElementById("same").innerHTML="비밀dfs번호가 일치하지 않습니다.";
     var pw = document.getElementById("userpassword").value;
     var pw2 = document.getElementById("userpassword2").value;
-    if (pw.length < 12 || pw.length >= 25) {
+    if ( (pw.length < 12 || pw.length >= 25) && (pw2.length < 12 || pw2.length >= 25) ) {
         window.alert("비밀번호는 12글자 이상, 25글자 이하만 이용 가능 합니다.");
         document.getElementById("userpassword").value = document.getElementById("userpassword2").value = "";
-        document.getElementById("same").innerHTML = "으아아아아";
+        document.getElementById("same").innerHTML = "12글자 이상 25글자 이하로 입력해주세요.";
+        document.getElementById("same").style="red";
     }
     if(document.getElementById("userpassword").value!="" && document.getElementById("userpassword2").value!=""){
-        if(document.getElementById(pw).value == document.getElementById(pw2).value){
-            document.getElementById("same").innerHTML='비밀번호가 일치합니다.';
+        if(document.getElementById("userpassword").value == document.getElementById("userpassword2").value){
+            document.getElementById("same").innerHTML="비밀번호가 일치합니다.";
             document.getElementById("same").style="blue";
         }
         else {
-            document.getElementById("same").innerHTML='비밀번호가 일치하지 않습니다.';
+            document.getElementById("same").innerHTML="비밀번호가 일치하지 않습니다.";
             document.getElementById("same").style="red";
         }
     }
