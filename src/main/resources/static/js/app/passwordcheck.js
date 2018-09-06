29d0f2162b16ac3e10d3558332e7908fca914045
Koobonik <kbi960130@naver.com>
2018-09-06 10:38:47 +0900

    코드 수정

--- src/main/resources/static/js/app/passwordcheck.js
+++ src/main/resources/static/js/app/passwordcheck.js
@@ -3,11 +3,11 @@ function check() {
     var pw2 = document.getElementById('userpassword2').value;
     if (pw.length <= 10 || pw.length >= 25) {
         window.alert("비밀번호는 6글자 이상, 16글자 이하만 이용 가능 합니다.");
-        document.getElementById('pw').value = document.getElementById('password2').value = '';
-        document.getElementById('same').innerHTML = '';
+        document.getElementById("userpassword").value = document.getElementById('userpassword2').value = '';
+        document.getElementById('same').innerHTML = '으아아아아';
     }
-    if(document.getElementById('pw').value!='' && document.getElementById('password2').value!=''){
-        if(document.getElementById('pw').value==document.getElementById('password2').value){
+    if(document.getElementById("userpassword").value!='' && document.getElementById("userpassword2").value!=''){
+        if(document.getElementById(pw).value == document.getElementById(pw2).value){
             document.getElementById('same').innerHTML='비밀번호가 일치합니다.';
             document.getElementById('same').style='blue';
         }
