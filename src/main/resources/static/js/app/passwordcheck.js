f3c835f8c2a7090bce3ff19b255b2e5d9816cc1a
Koobonik <kbi960130@naver.com>
2018-09-06 11:45:57 +0900

    주석 추가

--- src/main/resources/static/js/app/passwordcheck.js
+++ src/main/resources/static/js/app/passwordcheck.js
@@ -1,17 +1,25 @@
 function check() {
-    var pw = document.getElementById("userpassword").value;
-    var pw2 = document.getElementById("userpassword2").value;
+    var pw = document.getElementById("userpassword").value; // 변수의 크기 측정을 위해 사용될 변수
+    var pw2 = document.getElementById("userpassword2").value; // 변수의 크기 측정을 위해 사용될 변수
+
+    // 비밀번호의 크기는 12글자 이상 25글자 이하면 가능! 조금 줄일까 생각중
     if ( (pw.length < 12 || pw.length >= 25) && (pw2.length < 12 || pw2.length >= 25) ) {
         window.alert("비밀번호는 12글자 이상, 25글자 이하만 이용 가능 합니다.");
         document.getElementById("userpassword").value = document.getElementById("userpassword2").value = "";
         document.getElementById("same").innerHTML = "12글자 이상 25글자 이하로 입력해주세요.";
         document.getElementById("same").style.color="red";
     }
+
+    // 공백이 아니라면
     if(document.getElementById("userpassword").value!="" && document.getElementById("userpassword2").value!=""){
+
+        // 같다면 일치한다고 출력
         if(document.getElementById("userpassword").value == document.getElementById("userpassword2").value){
             document.getElementById("same").innerHTML="비밀번호가 일치합니다.";
             document.getElementById("same").style.color="blue";
         }
+
+        // 아니라면 일치하지 않는다고 출력
         else {
             document.getElementById("same").innerHTML="비밀번호가 일치하지 않습니다.";
             document.getElementById("same").style.color="red";
