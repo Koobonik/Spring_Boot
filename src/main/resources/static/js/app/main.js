72703d9962717f0cadfa15c57e3faa9abd78966c
Koobonik <kbi960130@naver.com>
2018-09-27 01:44:03 +0900

    주석추가

--- src/main/resources/static/js/app/main.js
+++ src/main/resources/static/js/app/main.js
@@ -17,7 +17,7 @@ var main = { // main 이라는 객체를 만드는 것임.
             url: '/posts', // 이게 중요함
             dataType: 'json',
             contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
+            data: JSON.stringify(data) // data 를 이쪽으로 보냄
         }).done(function() {
             alert('글이 등록되었습니다.');
             location.reload();
